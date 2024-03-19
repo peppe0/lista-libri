6df4110303488f081a760755db542304b5ddb9b3
peppe0 <peppe.abbatiello@hotmail.it>
2024-03-19 20:00:25 +0100

    first commit - created file project

---
+++ LMS.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string fileName = "libri.txt";
+    string input;
+    getline(cin,input);
+    cout << input << endl;
+    return 0;
+}
