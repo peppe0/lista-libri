d34cbc9b9d8b02d7e8d5c2691f29b05f26ec67f6
peppe0 <peppe.abbatiello@hotmail.it>
2024-03-19 22:16:31 +0100

    secondo commit - creazione file

--- LMS.cpp
+++ LMS.cpp
@@ -6,6 +6,13 @@ using namespace std;
 int main(){
     string fileName = "libri.txt";
     string input;
+
+    ofstream outFile(fileName); // Create ofstream object for writing
+    if (!outFile.is_open()) {
+        cerr << "Error: Unable to open file for writing!" << endl;
+        return 1;
+    }
+
     getline(cin,input);
     cout << input << endl;
     return 0;
